d7494ae0185d3e57181b9baf0a496ab37d35b21f
hhmbo <hhmbo@PC>
2024-02-29 16:32:19 +0100

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+Console.WriteLine("Welkomen!\ndu har kommit i FlowControl,\nDu " +
+  " komma att navigera genom att skriva in siffror");
